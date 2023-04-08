23bdc52f58ffec42c1f7c604ec3dcfce5d5fbe87
mugambi645 <mugambi645@gmail.com>
2023-04-08 09:01:12 +0300

    create components

---
+++ src/components/InputAmount.jsx
@@ -0,0 +1,10 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const InputAmount = () => {
+  return (
+    <Grid item>Item</Grid>
+  )
+}
+
+export default InputAmount
---
+++ src/components/SwitchCurrency.jsx
@@ -0,0 +1,10 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const SwitchCurrency = () => {
+  return (
+    <Grid item>Item</Grid>
+  )
+}
+
+export default SwitchCurrency
